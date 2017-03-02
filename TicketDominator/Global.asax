845421558979d192ca52549b7e00781d265cdf8e
mak001 <mak001001@gmail.com>
2017-03-02 12:11:04 -0600

    Started project

---
+++ TicketDominator/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TicketDominator.MvcApplication" Language="C#" %>
---
+++ TicketDominator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TicketDominator.Startup))]
+namespace TicketDominator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
