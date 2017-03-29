54f203d4aa8800d7c3075ff6cd5ab15d26a2f371
mak001 <mak001001@gmail.com>
2017-03-29 14:19:25 -0500

    Added Order details (minor adjustment to css)

---
+++ TicketDominator/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using TicketDominator.Models;
+
+namespace TicketDominator.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult Details(int id) {
+			Guid UserID = UserHelper.GetUserId();
+			ViewBag.ApplicationUser = UserHelper.GetApplicationUser();
+
+			using (TicketDominatorContext context = new TicketDominatorContext()) {
+				var order = context.Orders
+					.Include(p => p.OrderDetails.Select(c => c.Ticket))
+					.FirstOrDefault(x => x.Id == id && x.UserId == UserID);
+				return View(order);
+			}
+		}
+    }
+}
